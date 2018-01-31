5c1b9c8d851972304f17ca43f0c9f058fb739e77
Andy Maskiell <maskiella@gmail.com>
2018-01-31 17:22:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(row[1], row[2])
-    student
+    student = self.new(row[0], row[1], row[2])
   end
 
   def self.find_by_name
